247810d809fe8d8e70ee4d2a97f8ae4f241a9db7
tgolding <tpg@blueyonder.co.uk>
2016-11-24 23:54:14 +0000

    Added some more unit tests

--- src/test/scala/CheckoutTest.scala
+++ src/test/scala/CheckoutTest.scala
@@ -3,7 +3,7 @@ import shopcart.{Checkout, Price}
 
 class CheckoutTest extends FunSpec with GivenWhenThen with Matchers {
   describe("checkout") {
-    it("calculates the total cost of a list of items") {
+    it("calculates the total cost of a list of apples and oranges") {
       Given("a list of apples and oranges")
       val checkout = new Checkout
       val items = Seq("Apple", "Apple", "Orange", "Apple")
@@ -14,6 +14,45 @@ class CheckoutTest extends FunSpec with GivenWhenThen with Matchers {
       Then("it calculates the total cost of the items")
       actual should be(Price(205))
       actual.toString should be("£2.05")
+    }
+
+    it("calculates the total cost of a list of apples") {
+      Given("a list of apples")
+      val checkout = new Checkout
+      val items = Seq("Apple", "Apple")
+
+      When("the checkout receives a list of items")
+      val actual = checkout.calculateTotalCost(items)
+
+      Then("it calculates the total cost of the items")
+      actual should be(Price(120))
+      actual.toString should be("£1.20")
+    }
+
+    it("calculates the total cost of a list of oranges") {
+      Given("a list of oranges")
+      val checkout = new Checkout
+      val items = Seq("Orange", "Orange", "Orange")
+
+      When("the checkout receives a list of items")
+      val actual = checkout.calculateTotalCost(items)
+
+      Then("it calculates the total cost of the items")
+      actual should be(Price(75))
+      actual.toString should be("£0.75")
+    }
+
+    it("calculates the total cost of a empty list") {
+      Given("an empty list")
+      val checkout = new Checkout
+      val items = Seq()
+
+      When("the checkout receives a list of items")
+      val actual = checkout.calculateTotalCost(items)
+
+      Then("it calculates the total cost of the items")
+      actual should be(Price(0))
+      actual.toString should be("£0.00")
     }
   }
 }
