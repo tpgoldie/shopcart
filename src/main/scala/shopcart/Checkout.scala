f993b2f72a85e030a08e546496d824b7c48a026d
tgolding <tpg@blueyonder.co.uk>
2016-11-25 00:13:52 +0000

    Added another unit test

--- src/main/scala/shopcart/Checkout.scala
+++ src/main/scala/shopcart/Checkout.scala
@@ -1,12 +1,23 @@
 package shopcart
 
+import shopcart.Checkout.Zero
+
 class Checkout {
   val products = Seq(Apple, Orange)
 
   def calculateTotalCost(items: Seq[String]): Price = {
-    def selectProduct(item: String): Product = products.filter( _.name == item.toLowerCase ).head
+    def selectProduct(item: String): Price = {
+      products.filter( _.name == item.toLowerCase ) match {
+        case x::y => x.price
+        case List() => Zero
+      }
+    }
 
-    val prices = items.map { selectProduct(_).price }
-    prices.fold(Price(0))(_ + _)
+    val prices = items.map { selectProduct _ }
+    prices.fold(Zero)(_ + _)
   }
+}
+
+object Checkout {
+  val Zero= Price(0)
 }
--- src/test/scala/CheckoutTest.scala
+++ src/test/scala/CheckoutTest.scala
@@ -3,7 +3,7 @@ import shopcart.{Checkout, Price}
 
 class CheckoutTest extends FunSpec with GivenWhenThen with Matchers {
   describe("checkout") {
-    it("calculates the total cost of a list of apples and oranges") {
+    it("calculates the total cost of a list of just apples and oranges") {
       Given("a list of apples and oranges")
       val checkout = new Checkout
       val items = Seq("Apple", "Apple", "Orange", "Apple")
@@ -53,6 +53,19 @@ class CheckoutTest extends FunSpec with GivenWhenThen with Matchers {
       Then("it calculates the total cost of the items")
       actual should be(Price(0))
       actual.toString should be("£0.00")
+    }
+
+    it("calculates the total cost of a list of apples and oranges and no other products") {
+      Given("a list of apples, oranges, and bananas")
+      val checkout = new Checkout
+      val items = Seq("Apple", "Apple", "Orange", "Banana", "Apple", "Orange", "Banana")
+
+      When("the checkout receives a list of items")
+      val actual = checkout.calculateTotalCost(items)
+
+      Then("it calculates the total cost of the items")
+      actual should be(Price(230))
+      actual.toString should be("£2.30")
     }
   }
 }
