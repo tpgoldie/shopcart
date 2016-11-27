a58a63f2ac4fe96faf4a8baa3d163e1e6c2ab3f0
tgolding <tpg@blueyonder.co.uk>
2016-11-27 22:16:03 +0000

    Minor refactorings

--- src/main/scala/shopcart/Offer.scala
+++ src/main/scala/shopcart/Offer.scala
@@ -13,9 +13,7 @@ case class BuyOneGetOneFree(override val product: Product) extends Offer with Co
       case Orange => OrangeFilter(items)
     }
 
-    val products =  productFilter.filteredProducts
-
-    val size = products.size
+    val size =  productFilter.filteredProducts.size
 
     val quotient = size / 2
     val quotientIsPositive = quotient > 0
@@ -25,9 +23,7 @@ case class BuyOneGetOneFree(override val product: Product) extends Offer with Co
 
     val price = (quotientIsPositive, remainderIsPositive) match {
       case (false, false) => Zero
-      case (false, true) => product.price
-      case (true, false) => product.price * quotient
-      case (true, true) => product.price * (quotient+1)
+      case _ => product.price * (quotient+remainder)
     }
 
     price
